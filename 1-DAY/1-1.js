893a244019a0c30d7708b1a0a78b6bc1611de927
OnurKISACIK <onurkisacik1985@gmail.com>
2024-10-20 19:24:37 +0300

    30DAY CHALLANGE DAY 1

---
+++ 1-DAY/1-1.js
@@ -0,0 +1,13 @@
+let youTubeChannelName = "prototurk";
+let channelAge = 15;
+let channelActivity = true;
+let secondYoutubeChannel = null;
+let channelMoney = undefined;
+console.log(
+  typeof youTubeChannelName,
+  channelAge,
+  channelActivity,
+  secondYoutubeChannel,
+  channelMoney
+);
+console.log(typeof channelAge);

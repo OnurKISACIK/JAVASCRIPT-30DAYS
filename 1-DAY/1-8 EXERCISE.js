9342a079402d61a523b149101dbe3be7683dc6dd
OnurKISACIK <onurkisacik1985@gmail.com>
2024-10-22 23:30:48 +0300

    1ST DAY

---
+++ 1-DAY/1-8 EXERCISE.js
@@ -0,0 +1,65 @@
+//* 1- Write a single line comment which says, comments can make code readable
+
+// Comments can make the code readable
+
+//* 2- Write another single comment which says, Welcome to 30Days Challange
+
+// Welcome to 30days Challange
+
+//* 3- Write a multiline comment which says, Comment can make the understand the codes much more easy
+
+/*
+Comment can make the understand 
+the codes much more easy
+*/
+//* 4- Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+let newCarModel = "Mercedes";
+let exportDate = 2022;
+let anyIssue = false;
+let firstOwner = null;
+let costCar = undefined;
+
+//* 5- Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+let myCar = "Audi";
+console.log(typeof myCar);
+let myAge = 20;
+console.log(typeof myAge);
+let lightIsOn = true;
+console.log(typeof lightIsOn);
+let myWallet = null;
+console.log(typeof myWallet);
+let newGf;
+console.log(typeof newGf);
+
+//* 6- Declare four variables without assigning values
+let futballTeam;
+let basketballTeam;
+let myWay;
+let myChain;
+
+//* 7- Declare four variables with assigned values
+
+let client_Name = "David";
+let client_Age = 20;
+let client_City = "U.S.A";
+let client_Address = "20 Avenue 24587";
+
+//* 8- Declare variables to store your first name, last name, marital status,
+//*    country and age in multiple lines
+let myFirstName1 = "Tom";
+let myLastName1 = "Back";
+let myMaritalStatus11 = "Merried";
+let myAge11 = 35;
+
+//* 9- Declare variables to store your first name, last name, marital status, country and age in a single line
+
+let myFirstName2 = "Tom",
+  myLastName2 = "Back",
+  myMaritalStatus2 = "Merried",
+  myAge2 = 35;
+
+//* 10- Declare two variables myAge and yourAge and assign them initial values and log to the browser console
+
+let myAge3 = 20;
+let yourAge3 = 33;
